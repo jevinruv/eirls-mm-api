260dab2e1a5aec5a10385917ff50d4bd40987a88
Jevin Fernando <jevinruv@gmail.com>
2019-03-31 08:08:22 +0530

    code cleanup

--- src/main/java/com/jevin/eirlsmmapi/security/JwtAuthTokenFilter.java
+++ src/main/java/com/jevin/eirlsmmapi/security/JwtAuthTokenFilter.java
@@ -2,7 +2,6 @@
 package com.jevin.eirlsmmapi.security;
 
 import com.jevin.eirlsmmapi.service.UserDetailsServiceImpl;
-import io.jsonwebtoken.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
